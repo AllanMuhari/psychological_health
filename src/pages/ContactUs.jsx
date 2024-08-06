11a0c110cdac6c9c00c0ff6bcd7e54ea2be3a83b
AllanMuhari <allanmuhari087@gmail.com>
2024-08-06 20:19:37 +0300

    added 3 main services in home page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -16,10 +16,13 @@ const ContactUs = () => {
 
           <div className="w-full md:w-1/2 m-auto">
             <iframe
-              className="w-full h-80 rounded-lg shadow-lg"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127649.42611620306!2d36.82194622642484!3d-1.292065851959406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1f5e0c89144b%3A0x24a6d4761ae04c34!2sNairobi%2C%20Kenya!5e0!3m2!1sen!2sus!4v1682045155571!5m2!1sen!2sus"
-              allowFullScreen
-              loading="lazy"></iframe>
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8503520894387!2d36.8224386!3d-1.2621193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f171bdb71cc6b%3A0x645feec039f44849!2s3rd%20Park%20Hospital!5e0!3m2!1sen!2sde!4v1722962836189!5m2!1sen!2sde"
+              width="600"
+              height="450"
+              style="border:0;"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </main>
